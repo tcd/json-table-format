d94fc07f00117eec304ff0b03c27f1b5e973f65d
Clay Dunston <claydunston@gmail.com>
2022-01-09 10:11:49 -0600

    Remove unused code

--- test/tests/getKeys.test.ts
+++ test/tests/getKeys.test.ts
@@ -9,19 +9,16 @@ import * as cases from "@cases"
 describe("getKeys()", function () {
     it("case 1 - OBJECT", function () {
         let { input, keys } = cases.CASE_1
-        const want = keys
         const have = getKeys_object(JSON.parse(input))
         assert.sameMembers(have, keys)
     })
     it("case 2 - ARRAY", function () {
         let { input, keys } = cases.CASE_2
-        const want = keys
         const have = getKeys_object(JSON.parse(input))
         assert.sameMembers(have, keys)
     })
     it("case 3 - OBJECT", function () {
         let { input, keys } = cases.CASE_3
-        const want = keys
         const have = getKeys_object(JSON.parse(input))
         assert.sameMembers(have, keys)
     })
